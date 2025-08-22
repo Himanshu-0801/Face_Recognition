5a039a6821a457bbb5da1b100cd42bacddd32175
Himanshu-0801 <himanshu7412yadav@gmail.com>
2025-08-22 13:11:50 +0530

    database is set

--- dataset_creater.py
+++ dataset_creater.py
@@ -9,4 +9,15 @@ cam= cv2.VideoCapture(0) # to start the camera
 
 def insertorupdate(ID,Name,age):
     conn= sqlite3.connect("FaceBase.db") # connecting to the database
-    
+    cmd  = "SELECT * FROM STUDENT WHERE ID="+str(ID)
+    cursor = conn.execute(cmd)
+    isRecordExist = 0  # to check if record exists
+    for row in cursor:
+        isRecordExist = 1
+    if isRecordExist == 1:
+        cmd = ("UPDATE STUDENT SET Name=? WHERE ID=?", (Name, ID))
+        cmd = ("UPDATE STUDENT SET  Age=? WHERE ID=?", (age, ID))
+    else: # if record does not exists
+        conn.execute("INSERT INTO STUDENT(ID,Name,Age) VALUES(?,?,?)", (ID, Name, age))
+    conn.commit()
+    conn.close() 
